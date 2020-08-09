c8aed967d5bbeccb2ec3214fae47b4f792335bf0
roshangrewal <roshangrewal@hotmail.com>
2020-08-09 20:35:13 +0530

    routes and view engine setup - hbs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 // Load config
@@ -10,6 +12,21 @@ connectDB();
 
 // Initialized app
 const app = express();
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebars Template Engine
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// Static Folders
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 3000;
 
